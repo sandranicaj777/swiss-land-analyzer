d647b5b1b55216421b5bb2d8be2570d993753951
sandranicaj777 <aleksandraa.n777@gmail.com>
2025-11-17 22:29:14 +0100

    Added parcel route with fake sample data

--- app/main.py
+++ app/main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "SwissParcel backend is running"}
+
+
+#fake data
+@app.get("/parcels/{parcel_id}")
+def get_parcel(parcel_id: str):
+    sample_parcel = {
+        "id": 1,
+        "canton": "FR",
+        "municipality": "Exampleville",
+        "area_m2": 1234,
+        "zoning": "buildable",
+        "is_buildable": True,
+        "estimated_value_chf": 250_000,
+    }
+    return sample_parcel
